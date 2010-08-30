f3ab905d99556df9283e358889d0c7939d38387c
Philip Hallstrom <philip@pjkh.com>
2010-08-30 13:50:18 -0700

    add functionality to allow always showing of 'copy to clipboard' label text via label_visible=0|1 flash variable.

--- clippy.hx
+++ clippy.hx
@@ -1,3 +1,4 @@
+import flash.display.Bitmap;
 import flash.display.MovieClip;
 import flash.events.MouseEvent;
 import flash.display.SimpleButton;
@@ -5,10 +6,19 @@ import flash.text.TextField;
 import flash.text.TextFieldAutoSize;
 import flash.text.TextFormat;
 
+@:bind class Button_up extends flash.display.MovieClip {}
+@:bind class Button_down extends flash.display.MovieClip {}
+@:bind class Button_over extends flash.display.MovieClip {}
+
+
 class Clippy {
   // Main
   static function main() {
     var text:String = flash.Lib.current.loaderInfo.parameters.text;
+    var label_visible = Std.parseInt(flash.Lib.current.loaderInfo.parameters.label_visible);
+
+    flash.Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+    flash.Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
     
     // label
     
@@ -20,7 +30,7 @@ class Clippy {
     label.textColor = 0x888888;
     label.selectable = false;
     label.x = 15;
-    label.visible = false;
+    label.visible = (label_visible == 1);
     
     flash.Lib.current.addChild(label);
     
@@ -28,10 +38,10 @@ class Clippy {
     
     var button:SimpleButton = new SimpleButton();
     button.useHandCursor = true;
-    button.upState = flash.Lib.attach("button_up");
-    button.overState = flash.Lib.attach("button_over");
-    button.downState = flash.Lib.attach("button_down");
-    button.hitTestState = flash.Lib.attach("button_down");
+    button.upState = flash.Lib.attach("Button_up");
+    button.overState = flash.Lib.attach("Button_over");
+    button.downState = flash.Lib.attach("Button_down");
+    button.hitTestState = flash.Lib.attach("Button_down");
     
     button.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) {
       flash.system.System.setClipboard(text);
@@ -45,6 +55,7 @@ class Clippy {
     
     button.addEventListener(MouseEvent.MOUSE_OUT, function(e:MouseEvent) {
       label.visible = false;
+      label.visible = (label_visible == 1);
       label.text = "copy to clipboard";
       label.setTextFormat(format);
     });
